ec16cd1ec0c8c17cc628e754106aa00c4304627a
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-03 12:58:08 +0530

    changed btn

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,7 +3,6 @@ const Button =({href, txt}) =>{
     return (
         <div className="btn">
             <a href={href} className="btn-link">
-             {/* <span>{txt}</span> */}
              {txt}
             </a>
             
