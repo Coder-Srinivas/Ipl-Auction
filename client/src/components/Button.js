b4b5bd449748b55f814eae052dbcf9482ab2e498
FireQueen-3010 <dikshapatro807@gmail.com>
2021-05-03 12:56:41 +0530

    Button component changes

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,7 +3,8 @@ const Button =({href, txt}) =>{
     return (
         <div className="btn">
             <a href={href} className="btn-link">
-             <span>{txt}</span>
+             {/* <span>{txt}</span> */}
+             {txt}
             </a>
             
         </div>
