6d31e684fceb47a24e9317145bf29381b34bf248
Coder-Srinivas <srinivas.babu364@gmail.com>
2021-09-11 19:22:39 +0530

    Backend Test - 2

--- client/src/services/news.service.js
+++ client/src/services/news.service.js
@@ -2,6 +2,8 @@ import axiosInstance from "../utilities/axiosInstance";
 const url = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000/";
 
 const fetchNews = async () => {
+
+  console.log(process.env.NODE_ENV);
   return await axiosInstance.get(url + "news").then((response) => {
     return response.data.results;
   });
